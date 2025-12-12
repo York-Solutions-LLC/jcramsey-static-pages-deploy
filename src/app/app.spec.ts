bbb00a1394b844fc8e0d6d18da9cf85b0f8dc7ee
Joseph Cramsey <jcramseyc@yorksolutions.net>
2025-12-12 15:14:38 -0500

    Tests failed successfully

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, static-site-test');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, jcramsey-static-deploy-pages');
   });
 });
